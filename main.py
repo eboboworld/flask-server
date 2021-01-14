c4bc07ccd7ee344ea840dde07468733cf339e0fa
eboboworld <vitya201113@gmai.com>
2021-01-14 13:11:03 +0200

    new flask server create

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+menu = [
+	{'name': 'Установка', 'url': 'install-flask'},
+	{'name': 'Перша програма', 'url': 'first-app'},
+	{'name': 'Зворотній звязок', 'url': 'contact'}
+]
+
+@app.route('/')
+def index():
+	print( url_for('index'))
+	return render_template("index.html", menu=menu)
+
+@app.route('/about')
+def about():
+	print( url_for('about'))
+	return render_template("about.html", menu=menu)
+
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
